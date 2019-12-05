fc56a1e321b22e11e6fdd8222b58b2721f1d7965
stevekutz <stkutz@gmail.com>
2019-12-04 22:27:41 -0600

    completed selection sort & bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,6 @@
 # TO-DO: Complete the selection_sort() function below 
+import timeit
+
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -6,22 +8,43 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+        for j in range(i, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+   
+        # TO-DO: swap (used tuple packing/unpacking)
+        swap = arr[cur_index], arr[smallest_index]
+        arr[smallest_index], arr[cur_index] = swap
+        print(arr)
 
     return arr
+print(f'SELECTION SORT !!!')
+testarr = [3,44,38,5,47,15,36,26,27,2,46,4,19,50, 48]
+# elapsed_time = timeit.timeit( (selection_sort(testarr)), number = 100)/100
+# elapsed_time = timeit.timeit(selection_sort(testarr), number=100)/100
+print( selection_sort(testarr) )
+# print('Selection sort took:',  elapsed_time)
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
 
+def bubble_sort( arr ):
+    swapped = True
+    iteration = len(arr) - 1
+    while swapped and iteration > 0:
+        swapped: False
+        for i in range(0, len(arr)- 1):
+            if arr[i] > arr[i + 1]:
+                swap = arr[i], arr[i + 1]
+                arr[i + 1], arr[i] = swap
+                swapped: True
+        iteration -= 1
+        print(arr)
     return arr
+
+print(f'BUBBLE SORT ')
+testarr = [3,44,38,5,47,15,36,26,27,2,46,4,19,50, 48]
+print(bubble_sort(testarr))
 
 
 # STRETCH: implement the Count Sort function below
